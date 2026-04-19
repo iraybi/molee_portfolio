68c8fd22047b7b295f61463ef8bf648e6a8793ff
iraybi <iiraybi@gmail.com>
2026-04-20 01:40:59 +0600

    Switch base to / for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/molee_portfolio/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 });
